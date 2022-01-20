dcc760d114b263760f2e70bbdd8b9262b67ba2c2
Cloudftw <cloudninjakip@gmail.com>
2022-01-20 05:56:47 -0500

    Update postAugs.js

--- postAugs.js
+++ postAugs.js
@@ -1,10 +1,10 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	ns.exec(`hackandgrowtrigger.js,the-hub`)
-	ns.exec(`hackandgrowtrigger.js,joesguns`)
-	ns.exec(`hackandgrowtrigger.js,4sigma`)
-	ns.exec(`hackandgrowtrigger.js,n00dles`)
-	ns.exec(`hackandgrowtrigger.js,blade`)
-	ns.exec(`hackandgrowtrigger.js,fulcrumtech`)
-	ns.exec(`hackandgrowtrigger.js,megacorp`)
+	ns.exec(`hackandgrowtrigger.js,home,t,the-hub`)
+	ns.exec(`hackandgrowtrigger.js,home,t,joesguns`)
+	ns.exec(`hackandgrowtrigger.js,home,t,4sigma`)
+	ns.exec(`hackandgrowtrigger.js,home,t,n00dles`)
+	ns.exec(`hackandgrowtrigger.js,home,t,blade`)
+	ns.exec(`hackandgrowtrigger.js,home,t,fulcrumtech`)
+	ns.exec(`hackandgrowtrigger.js,home,t,megacorp`)
 }
